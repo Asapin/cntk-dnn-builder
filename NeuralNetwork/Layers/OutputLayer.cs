ac21a1726146462b8bcc9449d8434c3c6d1b50a4
Asapin <damnedvik@gmail.com>
2018-01-22 07:32:11 +0400

    New output layer

---
+++ NeuralNetwork/Layers/OutputLayer.cs
@@ -0,0 +1,33 @@
+﻿using CNTK;
+
+namespace NeuralNetwork.Layers
+{
+    public class OutputLayer : ILayer
+    {
+        private readonly int _outputDimesion;
+
+        public OutputLayer(int outputDimesion)
+        {
+            _outputDimesion = outputDimesion;
+        }
+
+        public Function Layer(ref Function input, ref DeviceDescriptor device)
+        {
+            var glorotInit = CNTKLib.GlorotUniformInitializer(
+                CNTKLib.DefaultParamInitScale,
+                CNTKLib.SentinelValueForInferParamInitRank,
+                CNTKLib.SentinelValueForInferParamInitRank, 1);
+            
+            var inputVar = (Variable) input;
+
+            var shape = new[] { _outputDimesion, inputVar.Shape[0] };
+            var weightParam = new Parameter(shape, DataType.Float, glorotInit, device, "weight");
+            var biasParam = new Parameter(new NDShape(1 ,_outputDimesion), 0, device, "bias");
+
+            var multiply = CNTKLib.Times(weightParam, inputVar);
+            var result = CNTKLib.Plus(multiply, biasParam);
+
+            return result;
+        }
+    }
+}
