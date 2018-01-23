2a83e8ea19c5c45ba153aa4cd70dd4ec96992143
Asapin <damnedvik@gmail.com>
2018-01-23 23:20:09 +0400

    New util class

---
+++ NeuralNetwork/Network/NetworkUtils.cs
@@ -0,0 +1,19 @@
+﻿using CNTK;
+
+namespace NeuralNetwork.Network
+{
+    public abstract class NetworkUtils
+    {
+        public static  MinibatchSource GetMinibatchSource(NetworkDescriptor descriptor, bool train, 
+            ref Variable features, ref Variable labels)
+        {
+            var streamConfig = new[]
+            {
+                new StreamConfiguration(descriptor.FeaturesStreamName, features.Shape[0]), 
+                new StreamConfiguration(descriptor.LabelsStreamName, labels.Shape[0]) 
+            };
+            return MinibatchSource.TextFormatMinibatchSource(train ? descriptor.TrainDataPath : descriptor.TestDataPath, 
+                streamConfig, MinibatchSource.InfinitelyRepeat, true);
+        }
+    }
+}
