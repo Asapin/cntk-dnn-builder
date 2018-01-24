b48cd82e4e1c83a34f90559ba695b2142bc8b1f2
Asapin <damnedvik@gmail.com>
2018-01-24 21:33:59 +0400

    Cleaned up main class

--- NeuralNetwork/Program.cs
+++ NeuralNetwork/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using NeuralNetwork.Layers;
-using NeuralNetwork.Network;
+using NeuralNetwork.PreparedModels;
 
 namespace NeuralNetwork
 {
@@ -8,33 +7,14 @@ namespace NeuralNetwork
     {
         public static void Main(string[] args)
         {
-            if (args.Length != 3)
+            if (args.Length != 1)
             {
-                Console.WriteLine("You should provide paths to a training data, to a testing data " +
-                                  "and to a folder for storing checkpoints");
+                Console.WriteLine("You should provide path to a folder for storing checkpoints");
                 return;
             }
 
-            var descriptor = new NetworkDescriptor(args[0], args[1], args[2], NetworkType.Onehot, 189, 3)
-            {
-                BatchSize = 100,
-                EpochCheckpoint = 10,
-                Epochs = 5000,
-                Evaluate = true,
-                FeaturesStreamName = "features",
-                LabelsStreamName = "labels",
-                LearningRatePerSample = 0.00125f
-            };
-
-            ILayer[] layers =
-            {
-                new DropoutLayer(0.3), 
-                new SimpleLayer(200, Activation.LeakyReLU),
-                new SimpleLayer(100, Activation.LeakyReLU),
-            };
-
-            var network = new Network.NeuralNetwork(layers, descriptor);
-            network.RunTraining();
+            var irisNetwork = new IrisNetwork(args[0]);
+            irisNetwork.Train();
         }
     }
 }
