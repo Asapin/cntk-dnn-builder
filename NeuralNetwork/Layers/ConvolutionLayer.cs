9f443c52cde813bdca24306e616f3cca5319af24
Asapin <damnedvik@gmail.com>
2018-03-04 22:45:45 +0400

    Fixes for convolutional layer

--- NeuralNetwork/Layers/ConvolutionLayer.cs
+++ NeuralNetwork/Layers/ConvolutionLayer.cs
@@ -10,9 +10,10 @@ namespace NeuralNetwork.Layers
         private readonly int _filtersCount;
         private readonly int _hStride;
         private readonly int _vStride;
+        private readonly bool _padding;
 
         public ConvolutionLayer(Activation.Apply activation, int kernelWidth, int kernelHeight, 
-            int filtersCount, int hStride = 1, int vStride = 1)
+            int filtersCount, int hStride = 1, int vStride = 1, bool padding = false)
         {
             _activation = activation;
             _kernelWidth = kernelWidth;
@@ -20,6 +21,7 @@ namespace NeuralNetwork.Layers
             _filtersCount = filtersCount;
             _hStride = hStride;
             _vStride = vStride;
+            _padding = padding;
         }
 
         public override Function Layer(ref Function input, ref DeviceDescriptor device)
@@ -29,10 +31,11 @@ namespace NeuralNetwork.Layers
 
             var numInputChannels = inputVar.Shape[inputVar.Shape.Rank - 1];
 
-            var convParams = new Parameter(new[] { _kernelWidth, _kernelHeight, numInputChannels, _filtersCount },
-                DataType.Float, glorotInit, device);
+            var convParams = new Parameter(new[] { _kernelWidth, _kernelHeight, numInputChannels, _filtersCount }, 
+                DataType.Float, glorotInit, device, "convParams");
 
-            var convFunction = CNTKLib.Convolution(convParams, inputVar, new[] { _hStride, _vStride, numInputChannels });
+            var convFunction = CNTKLib.Convolution(convParams, inputVar, new[] { _hStride, _vStride, numInputChannels }, 
+                new[] { true }, new[] { _padding });
 
             return _activation(convFunction);
         }
