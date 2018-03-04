3280cc4e0ad6a8f40396d2209f314fc15604bbba
Asapin <damnedvik@gmail.com>
2018-03-04 19:28:14 +0400

    Added new pooling layer

---
+++ NeuralNetwork/Layers/PoolingLayer.cs
@@ -0,0 +1,32 @@
+﻿using CNTK;
+
+namespace NeuralNetwork.Layers
+{
+    public class PoolingLayer : AbstractLayer
+    {
+        private readonly PoolingType _poolingType;
+        private readonly int _hFilterSize;
+        private readonly int _vFilterSize;
+        private readonly int _hStride;
+        private readonly int _vStride;
+        private readonly bool _padding;
+
+        public PoolingLayer(PoolingType poolingType, int hFilterSize, int vFilterSize, 
+            int hStride = 1, int vStride = 1, bool padding = false)
+        {
+            _poolingType = poolingType;
+            _hFilterSize = hFilterSize;
+            _vFilterSize = vFilterSize;
+            _hStride = hStride;
+            _vStride = vStride;
+            _padding = padding;
+        }
+
+        public override Function Layer(ref Function input, ref DeviceDescriptor device)
+        {
+            var inputVar = (Variable) input;
+            return CNTKLib.Pooling(inputVar, _poolingType, new[] { _hFilterSize, _vFilterSize }, 
+                new[] { _hStride, _vStride }, new[] { _padding });
+        }
+    }
+}
