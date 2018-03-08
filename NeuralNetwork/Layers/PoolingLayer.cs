5a0ba3fa227e337ac81ffde2d4b210ed2c4dedd6
Vladimir Kuzmin <v.kuzmin@simbirsoft.com>
2018-03-08 19:44:21 +0400

    Renamed parameters

--- NeuralNetwork/Layers/PoolingLayer.cs
+++ NeuralNetwork/Layers/PoolingLayer.cs
@@ -5,18 +5,18 @@ namespace NeuralNetwork.Layers
     public class PoolingLayer : AbstractLayer
     {
         private readonly PoolingType _poolingType;
-        private readonly int _hFilterSize;
-        private readonly int _vFilterSize;
+        private readonly int _kernelWidth;
+        private readonly int _kernelHeight;
         private readonly int _hStride;
         private readonly int _vStride;
         private readonly bool _padding;
 
-        public PoolingLayer(PoolingType poolingType, int hFilterSize, int vFilterSize, 
+        public PoolingLayer(PoolingType poolingType, int kernelWidth, int kernelHeight, 
             int hStride = 1, int vStride = 1, bool padding = false)
         {
             _poolingType = poolingType;
-            _hFilterSize = hFilterSize;
-            _vFilterSize = vFilterSize;
+            _kernelWidth = kernelWidth;
+            _kernelHeight = kernelHeight;
             _hStride = hStride;
             _vStride = vStride;
             _padding = padding;
@@ -25,7 +25,7 @@ namespace NeuralNetwork.Layers
         public override Function Layer(ref Function input, ref DeviceDescriptor device, string checkpointSavePath)
         {
             var inputVar = (Variable) input;
-            var result = CNTKLib.Pooling(inputVar, _poolingType, new[] { _hFilterSize, _vFilterSize }, 
+            var result = CNTKLib.Pooling(inputVar, _poolingType, new[] { _kernelWidth, _kernelHeight }, 
                 new[] { _hStride, _vStride }, new[] { _padding });
             
             LogShape(ref result, checkpointSavePath, "Pooling");
